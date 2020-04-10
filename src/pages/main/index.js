92b20236807d5cd9e3496fcde73bd91cf7d61461
Polinarka <saira6303@gmail.com>
2020-04-10 23:18:50 +0400

    поп-ап на главной заработал))

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,4 +1,4 @@
-var button = document.querySelector(".slide__link_js");
+var button = document.querySelector(".slider__link_js");
 console.log(button);
 var modalForm = document.querySelector(".modal-form_js");
 console.log(modalForm);
@@ -6,18 +6,18 @@ var buttonClose = document.querySelector(".button-closed_js");
 var input = document.querySelector(".form-input_js");
 
 button.addEventListener("click", function () {
-    modalForm.classList.remove("modal-form_none");
+    modalForm.classList.remove("contact-us__modal-form_none");
     input.focus();
     console.log();
 });
 
 buttonClose.addEventListener("click", function () {
-    modalForm.classList.add("modal-form_none");
+    modalForm.classList.add("contact-us__modal-form_none");
 });
 
 window.addEventListener("keydown", function (event) {
-    if(event.code == "Escape" && !modalForm.classList.contains("modal-form_none")) {
-    modalForm.classList.add("modal-form_none");
+    if(event.code == "Escape" && !modalForm.classList.contains("contact-us__modal-form_none")) {
+    modalForm.classList.add("contact-us__modal-form_none");
     button.focus();
     }
     });
